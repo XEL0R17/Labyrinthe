1f6b83202f28a1d1d414c096062e2f33c8abd3b3
XEL0R17 <195105@ecam.be>
2023-05-10 19:07:43 +0200

    simulate what i'm supposed to receive from the server

---
+++ simulation.py
@@ -0,0 +1,359 @@
+server_state = {
+    "players": ["A_star", "Adversaire"],
+    "current": 0,
+    "positions": [1, 6],
+    "target": 4,
+    "remaining": [4, 4],
+    "tile": {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+            },
+    "board": [
+        {
+            "N": False,
+            "E": False,
+            "S": False,
+            "W": False,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": False,
+            "E": False,
+            "S": False,
+            "W": False,
+            "item": None
+        },
+        {
+            "N": False,
+            "E": False,
+            "S": False,
+            "W": False,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": 4
+        },
+        {
+            "N": False,
+            "E": False,
+            "S": True,
+            "W": False,
+            "item": None
+        },
+        {
+            "N": False,
+            "E": False,
+            "S": True,
+            "W": False,
+            "item": None
+        },
+        {
+            "N": False,
+            "E": False,
+            "S": True,
+            "W": False,
+            "item": None
+        },
+        {
+            "N": False,
+            "E": False,
+            "S": True,
+            "W": False,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": 1
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": 2
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": 3
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        },
+        {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+            "item": None
+        }
+    ]
+}
