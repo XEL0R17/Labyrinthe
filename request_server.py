5da6db259db7530c68dfe7dd1e135b344b00458f
XEL0R17 <195105@ecam.be>
2023-05-11 09:37:47 +0200

    add move request

--- request_server.py
+++ request_server.py
@@ -1,5 +1,6 @@
 import socket
 import json
+from main import AI 
 
 HOST = 'localhost'
 PORT = 3000  # Replace with the port number the server is listening on
@@ -7,24 +8,23 @@ NAME = 'Champions'
 CLIENT_PORT = 5000  # This is the port the client will use for incoming connections
 MATRICULES = ["20089", "195105"]
 
-def handle_ping(conn):
+def handle_request(conn):
     data = conn.recv(2048)
     request = json.loads(data.decode())
     if request['request'] == 'ping':
         response = json.dumps({'response': 'pong'}).encode()
-        conn.send(response)
-        conn.close()
+    elif request['request'] == 'request move':
+        game_state = request['game_state']
+        ai = AI(game_state)
+        move = ai.play()
+        response = json.dumps({'response': 'move', 'move': move}).encode()
+    else:
+        response = json.dumps({'response': 'error', 'message': 'Unknown request'}).encode()
 
-def start_ping_server():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('0.0.0.0', CLIENT_PORT))
-        s.listen()
-        while True:
-            conn, addr = s.accept()
-            with conn:
-                handle_ping(conn)
+    conn.send(response)
+    conn.close()
 
-def main():
+def start_client():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
 
@@ -42,10 +42,14 @@ def main():
         response = json.loads(data.decode())
         print(response)
 
-if __name__ == '__main__':
-    main()
+    # Start ping server
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('0.0.0.0', CLIENT_PORT))
+        s.listen()
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                handle_request(conn)
 
-    try:
-        start_ping_server()
-    except KeyboardInterrupt:
-        print("Shutting down the client")
+if __name__ == '__main__':
+    start_client()
